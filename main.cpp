f18a4454efc4cc82f9598a17de55e3b41503dd30
Owen Ceresa <co84635@mail.harpercollege.edu>
2024-10-04 09:28:06 -0500

    printResultsFixed

--- main.cpp
+++ main.cpp
@@ -3,30 +3,30 @@
 
 using namespace std;
 
-bool isValidInput(int lower, int upper, int value) {
+bool is_valid_input(int lower, int upper, int value) {
     if (lower != INT_MIN && value < lower) return false;
     if (upper != INT_MAX && value > upper) return false;
     return true;
 }
 
-int menuValidIntegerChecker(string prompt, string errorMessage, int lower = INT_MIN, int upper = INT_MAX) {
+void print_result(int result) {
+    cout << "The value selected by the user: " << result << endl;
+}
+
+int menu_valid_integer_checker(string prompt, string errorMessage, int lower = INT_MIN, int upper = INT_MAX) {
     
     while(true) {
         int input;
         cout << prompt << endl;
         cin >> input;
 
-        if (isValidInput(lower, upper, input)) {
-            printResult(input);
+        if (is_valid_input(lower, upper, input)) {
+            print_result(input);
             return input;
         } else {
             cout << errorMessage << endl;
         }
     }
-}
-
-void printResult(int result) {
-    cout << "The value selected by the user: " << result << endl;
 }
 
 int main () {
@@ -35,23 +35,17 @@ int main () {
 
     int result;
 
-    result = menuValidIntegerChecker(prompt, errorMessage);
-    printResult(result);
-
-    result = menuValidIntegerChecker("Please enter a number (0-1000):", errorMessage, 0, 1000);
-    printResult(result);
-
-    result = menuValidIntegerChecker("Please enter a number < 500:", errorMessage, INT_MIN, 500);
-    printResult(result);
-
-    result = menuValidIntegerChecker("Please enter a number > -500:", errorMessage, -500, INT_MAX);
-    printResult(result);
-
-    result = menuValidIntegerChecker("Please enter a number larger > 7:", errorMessage, 7, INT_MAX);
-    printResult(result);
-
-    result = menuValidIntegerChecker("Please enter a number between -1000 and 1000:", errorMessage, -1000, 1000);
-    printResult(result);
-
+    result = menu_valid_integer_checker(prompt, errorMessage);
+    
+    result = menu_valid_integer_checker("Please enter a number (0-1000):", errorMessage, 0, 1000);
+  
+    result = menu_valid_integer_checker("Please enter a number < 500:", errorMessage, INT_MIN, 500);
+   
+    result = menu_valid_integer_checker("Please enter a number > -500:", errorMessage, -500, INT_MAX);
+   
+    result = menu_valid_integer_checker("Please enter a number larger > 7:", errorMessage, 7, INT_MAX);
+   
+    result = menu_valid_integer_checker("Please enter a number between -1000 and 1000:", errorMessage, -1000, 1000);
+  
     return 0;
 }
